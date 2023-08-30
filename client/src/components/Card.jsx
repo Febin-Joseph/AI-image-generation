2a23830d2f1799a1b6bfec2a31fb6e74465e3035
Febin-Joseph <febinachu123@gmail.com>
2023-08-30 05:35:11 +0530

    first commit

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { download } from '../assets';
+import { downloadImage } from '../utils';
+
+const Card = ({ _id, photo, prompt, name}) => {
+  return (
+    <div>Card</div>
+    // <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
+    //   <img className='w-full h-auto object-cover rounded-x' src={photo} alt={prompt} />
+
+    // </div>
+  )
+}
+
+export default Card
